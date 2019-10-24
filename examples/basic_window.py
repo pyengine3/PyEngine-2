05d4a2a17146dbce0e1b9a9552716b246b76f46f
LavaPower <lavapower84@gmail.com>
2019-10-24 14:22:04 +0200

    BasicEntity : Test ControlComponent

--- examples/basic_window.py
+++ examples/basic_window.py
@@ -2,5 +2,5 @@ from pyengine2 import Window
 from pyengine2.Utils import Color
 
 
-window = Window(250, 250, color=Color.from_name("navyblue"), debug=True)
+window = Window(250, 250, color=Color.from_name("NAVY_BLUE"), debug=True)
 window.run()
