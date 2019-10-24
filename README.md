bee1a08d32ba5b6ba5756b589c04b7eb01847462
LavaPower <lavapower84@gmail.com>
2019-10-24 11:38:20 +0200

    Create basic lib and repo

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PyEngine 2
- A new clean version of PyEngine using PyGame 2
+
+A new clean version of PyEngine using PyGame 2
+ 
+## Dependencies
+
+- Python 3.5+
+- PyGame 2
+
+## Installation
+
+- Install PyEngine 2 with `pip install PyEngine-2`
+- Enjoy
+
+## Features
+
+*Coming Soon*
+
+## Uncoming Features
+
+*Coming Soon*
+
+## Changelog
+
+### V 1.0.0 : XXX Update - XX/XX/XX (INDEV)
+
+- First version
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+import pyengine2
+
+setup(
+    name="PyEngine-2",
+    version=pyengine2.__version__,
+    packages=find_packages(),
+    author="LavaPower",
+    author_email="lavapowe84@gmail.com",
+    description="A new clean version of PyEngine using PyGame 2",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+
+    include_package_data = True,
+
+    url="http://github.com/pyengine-2D/PyEngine-2",
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: pygame",
+        "Intended Audience :: Developers",
+    ],
+    install_requires=['pygame==2.0.0.dev4'],
+)
