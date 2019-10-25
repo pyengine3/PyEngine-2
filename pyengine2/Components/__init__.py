5b51ddb732caae65523ec5a55c00d856a24896f5
LavaPower <lavapower84@gmail.com>
2019-10-26 01:21:22 +0200

    __init__

--- pyengine2/Components/__init__.py
+++ pyengine2/Components/__init__.py
@@ -2,3 +2,5 @@ from pyengine2.Components.SpriteComponent import SpriteComponent
 from pyengine2.Components.PositionComponent import PositionComponent
 from pyengine2.Components.ShowComponent import ShowComponent
 from pyengine2.Components.ControlComponent import ControlComponent
+from pyengine2.Components.AutoComponent import AutoComponent
+from pyengine2.Components.TextComponent import TextComponent
--- pyengine2/Utils/__init__.py
+++ pyengine2/Utils/__init__.py
@@ -1,3 +1,5 @@
 from pyengine2.Utils.Utils import clamp
 from pyengine2.Utils.Color import Color
 from pyengine2.Utils.Logger import logger
+from pyengine2.Utils.Vec2 import Vec2
+from pyengine2.Utils.Font import Font
