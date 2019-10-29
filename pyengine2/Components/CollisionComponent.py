5101a31cc94164e158cedb3e1b7227501466fbc7
LavaPower <lavapower84@gmail.com>
2019-10-29 14:48:48 +0100

    Add causes in CollisionComponent

--- pyengine2/Components/CollisionComponent.py
+++ pyengine2/Components/CollisionComponent.py
@@ -4,6 +4,8 @@ from pyengine2.Components.PositionComponent import PositionComponent
 
 
 class CollisionComponent(Component):
+    causes = ["UNKNOWN", "LEFTCONTROL", "RIGHTCONTROL", "UPCONTROL", "DOWNCONTROL", "AUTOCOMPONENT"]
+
     def __init__(self, callback=None, solid=True):
         """
             Create CollisionComponent
@@ -17,10 +19,11 @@ class CollisionComponent(Component):
         self.solid = solid
         self.callback = callback
 
-    def can_go(self, x, y):
+    def can_go(self, x, y, cause="UNKNOWN"):
         """
             Know if Entity can go to x, y position
 
+            :param cause: Cause de la collision (si elle existe)
             :param x: X Pos
             :param y: Y Pos
         """
@@ -31,7 +34,11 @@ class CollisionComponent(Component):
                 pos = entity.get_component(PositionComponent).position()
                 other_rect = entity.get_component(SpriteComponent).transformed_image.get_rect(x=pos.x, y=pos.y)
                 if rect.colliderect(other_rect):
+                    if cause not in self.causes:
+                        cause = "UNKNOWN"
+                    if self.callback:
+                        self.callback(entity, cause)
+
                     if entity.get_component(CollisionComponent).solid and self.solid:
                         return False
-                    self.callback(entity)
         return True
