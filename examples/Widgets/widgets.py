b194ae4dbdccff934e4a61f094e8921d61009a47
LavaPower <lavapower84@gmail.com>
2019-10-30 12:30:00 +0100

    Test button

--- examples/Widgets/widgets.py
+++ examples/Widgets/widgets.py
@@ -1,16 +1,17 @@
 from pyengine2 import Window
 from pyengine2.Entities import Entity
 from pyengine2.Components import PositionComponent, SpriteComponent, ShowComponent, ControlComponent
-from pyengine2.Widgets import Label
+from pyengine2.Widgets import *
 from pyengine2.Utils import Vec2
 
 import os
 
 images = [os.path.join(os.path.dirname(__file__), "sprite0.png")]
 
-game = Window(550, 300, debug=True)
+game = Window(750, 500, debug=True)
 
-l = Label(100, 100, "Test Label :D")
+l = Label(100, 100, "Label")
+b = Button(200, 200, "Button", lambda: print("BUTTON CLICKED"))
 
 e = Entity()
 
@@ -20,6 +21,7 @@ e.add_component(ShowComponent())
 e.add_component(ControlComponent("FOURDIRECTION"))
 
 game.world.ui_system.add_widget(l)
+game.world.ui_system.add_widget(b)
 game.world.entity_system.add_entity(e)
 
 game.run()
