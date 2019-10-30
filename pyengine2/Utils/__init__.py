901787fd3bc85d539b26af60c97d780d5ff08b30
LavaPower <lavapower84@gmail.com>
2019-10-30 17:50:42 +0100

    __init__

--- pyengine2/Utils/__init__.py
+++ pyengine2/Utils/__init__.py
@@ -3,3 +3,5 @@ from pyengine2.Utils.Color import Color
 from pyengine2.Utils.Logger import logger
 from pyengine2.Utils.Vec2 import Vec2
 from pyengine2.Utils.Font import Font
+from pyengine2.Utils.Config import Config
+from pyengine2.Utils.Lang import Lang
