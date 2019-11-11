2d02dec4fd43f8fd5965f50b280c1a5fea1737e0
LavaPower <lavapower84@gmail.com>
2019-11-11 16:59:29 +0100

    Update anim test

--- examples/Entities/anim_entities.py
+++ examples/Entities/anim_entities.py
@@ -1,16 +1,24 @@
 from pyengine2 import Window
 from pyengine2.Entities import Entity
-from pyengine2.Components import PositionComponent, SpriteComponent, ShowComponent, AnimListComponent
+from pyengine2.Components import PositionComponent, SpriteComponent, ShowComponent, AnimListComponent,\
+    AnimSheetComponent
 
 
 game = Window(200, 200, debug=True)
 
 e = Entity()
-e.add_component(PositionComponent(100, 100))
+e.add_component(PositionComponent(50, 50))
 e.add_component(SpriteComponent("sprite0.png"))
 e.add_component(AnimListComponent("sprite0.png", "sprite1.png"))
 e.add_component(ShowComponent())
 
+e2 = Entity()
+e2.add_component(PositionComponent(150, 150))
+e2.add_component(SpriteComponent("sprite0.png"))
+e2.add_component(AnimSheetComponent("spritesheet.png", 2))
+e2.add_component(ShowComponent())
+
 game.world.entity_system.add_entity(e)
+game.world.entity_system.add_entity(e2)
 
 game.run()
